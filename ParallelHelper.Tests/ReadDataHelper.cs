0325e40c1444c121db22f8c1775b136426ebcea2
Eric Hansel <e.hansel.01@gmail.com>
2016-03-10 20:13:49 -0500

    Initial Commit

---
+++ ParallelHelper.Tests/ReadDataHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ParallelHelper.Tests
+{
+    public class ReadDataHelper
+    {
+        public List<string> GetStrings()
+        {
+            return new List<string>() { "This is s string", "This is s string", "This is s string", "This is s string" };
+        }
+    }
+}
---
+++ ParallelHelper/ParallelProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelHelper
+{
+    public class ParallelProcessor<TObjectToProcess, TProcessedObject>
+    {
+        public List<TObjectToProcess> List1 { get; private set; }
+
+        public List<TObjectToProcess> List2 { get; private set; }
+
+        public Func<List<TObjectToProcess>> LoadData;
+
+        public void Start()
+        {
+            List1 = LoadData.Invoke();
+        }
+    }
+}
